2e81d3478d963fca9aea47b99432aef257027c1a
Alla Witte <alla_88@inbox.lv>
2020-10-28 14:50:24 -0300

    say hello

--- app.php
+++ app.php
@@ -1,5 +1,5 @@
 <?php
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 $app = new \Symfony\Component\Console\Application('demo app');
-
+$app->add(new App\SayHello());
 $app->run();
